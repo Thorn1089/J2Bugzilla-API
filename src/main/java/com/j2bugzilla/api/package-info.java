5b2aa1e1080d083351b3373bc945eb4752de1b1a
Tom Golden <TomRK1089@gmail.com>
2013-04-30 19:32:01 -0400

    Added the API package.

---
+++ src/main/java/com/j2bugzilla/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The J2Bugzilla API package groups classes which provide either concrete domain objects representing
+ * items in a Bugzilla installation, or abstract interfaces used to communicate with a given
+ * installation.
+ * 
+ * @author Tom
+ */
+package com.j2bugzilla.api;
