81b3a35818c516fc20b201af06a596a7bc52cb4b
Tom Golden <TomRK1089@gmail.com>
2013-04-30 19:40:01 -0400

    Added stub for BugRepository interface.

---
+++ src/main/java/com/j2bugzilla/api/BugRepository.java
@@ -0,0 +1,11 @@
+package com.j2bugzilla.api;
+
+/**
+ * The {@code BugRepository} interface defines a CRUD-style class which allows {@link Bug Bugs} to be
+ * created, modified, and searched from a particular Bugzilla installation.
+ * 
+ * @author Tom
+ */
+public interface BugRepository {
+
+}
