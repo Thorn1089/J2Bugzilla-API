44c45b154e0f0ed485876336b13824233778ac8e
Tom Golden <TomRK1089@gmail.com>
2013-04-30 22:25:05 -0400

    Added Javadocs for BugRepository.

--- src/main/java/com/j2bugzilla/api/BugRepository.java
+++ src/main/java/com/j2bugzilla/api/BugRepository.java
@@ -1,5 +1,9 @@
 package com.j2bugzilla.api;
 
+import java.util.Set;
+
+import com.google.common.base.Optional;
+
 /**
  * The {@code BugRepository} interface defines a CRUD-style class which allows {@link Bug Bugs} to be
  * created, modified, and searched from a particular Bugzilla installation.
@@ -8,4 +12,34 @@ package com.j2bugzilla.api;
  */
 public interface BugRepository {
 
+	/**
+	 * Creates a new {@link Bug} report in the associated installation, which has the properties described
+	 * by the given bug.
+	 * @param bug A {@code Bug} to create on a remote installation.
+	 */
+	void create(Bug bug);
+	
+	/**
+	 * Retrieves a {@link Bug} with the given ID. If no such bug is found, the return value is absent.
+	 * @param id An {@code int} identifying a bug.
+	 * @return An {@link Optional} reference to a {@code Bug}.
+	 */
+	Optional<Bug> get(int id);
+	
+	/**
+	 * Retrieves all {@link Bug Bugs} matching the given set of IDs. If a given ID does not match a bug, it
+	 * is ignored.
+	 * @param ids One or more {@code ints} identifying {@code Bugs}.
+	 * @return A {@code Set} of {@code Bug} objects.
+	 */
+	Set<Bug> getAll(int... ids);
+	
+	/**
+	 * Synchronizes the state of a {@link Bug} with the associated Bugzilla instance.
+	 * @param bug A {@code Bug} to update.
+	 */
+	void update(Bug bug);
+	
+	//TODO Search -- need to define a search params class
+	
 }
