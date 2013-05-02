e5931abec52a1d60ee2fe6468043c888edfa0744
Tom Golden <TomRK1089@gmail.com>
2013-05-02 08:17:15 -0400

    Added the CommentRepository interface.

---
+++ src/main/java/com/j2bugzilla/api/CommentRepository.java
@@ -0,0 +1,46 @@
+package com.j2bugzilla.api;
+
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.base.Optional;
+
+/**
+ * The {@code CommentRepository} allows consumers to retrieve {@link Comment comments}
+ * from existing {@link Bug} reports and add new comments to bugs.
+ * 
+ * @author Tom
+ */
+public interface CommentRepository {
+
+	/**
+	 * Adds a new comment to an existing bug.
+	 * @param bug A {@link Bug} to comment on.
+	 * @param comment A {@link Comment} including the text to add from the current user.
+	 */
+	void commentOn(Bug bug, Comment comment);
+	
+	/**
+	 * Retrieves the identified comment, or an absent {@link Optional} if no such comment exists.
+	 * @param id An {@code int} identifying a comment.
+	 * @return A {@link Comment} if one exists.
+	 */
+	Optional<Comment> get(int id);
+	
+	/**
+	 * Returns a {@code Set} of comments identified by the given IDs. If nonexistent IDs are specified,
+	 * they are ignored.
+	 * @param ids A set of {@code ints} identifying comments.
+	 * @return A set of {@link Comment} objects.
+	 */
+	Set<Comment> getAll(int... ids);
+	
+	/**
+	 * Gets an ordered {@code List} of comments representing the chronological thread of discusson on
+	 * a single bug.
+	 * @param bug A {@link Bug} to retrieve the discussion history for.
+	 * @return A list of {@link Comment} objects.
+	 */
+	List<Comment> getThreadFor(Bug bug);
+	
+}
