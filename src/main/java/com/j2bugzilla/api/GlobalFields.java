07da0a15205476e6a6b6918270c372e81733ff4a
Tom Golden <TomRK1089@gmail.com>
2013-05-18 16:56:41 -0400

    Added documentation to enumerations.

--- src/main/java/com/j2bugzilla/api/GlobalFields.java
+++ src/main/java/com/j2bugzilla/api/GlobalFields.java
@@ -8,11 +8,29 @@ package com.j2bugzilla.api;
  * @author Tom
  */
 public enum GlobalFields {
+	/**
+	 * Hardware platforms, such as 'PC' or 'Macintosh'.
+	 */
 	REP_PLATFORM("rep_platform"), 
+	/**
+	 * Operating systems, such as 'Windows 8' or 'OSX Mountain Lion'.
+	 */
 	OP_SYS("op_sys"), 
+	/**
+	 * Bug priorities, such as 'P1'.
+	 */
 	PRIORITY("priority"), 
+	/**
+	 * Bug severities, such as 'Major' or 'Severe'.
+	 */
 	SEVERITY("bug_severity"), 
+	/**
+	 * Bug statuses, such as 'Open' or 'In progress'.
+	 */
 	STATUS("bug_status"), 
+	/**
+	 * Bug outcomes, such as 'Fixed' or 'Needs confirmation'.
+	 */
 	RESOLUTION("resolution");
 	
 	private String internalName;
