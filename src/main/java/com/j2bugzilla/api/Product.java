fbf0c56730d4f7399122ac1a2f789cc3af768a4a
Tom Golden <TomRK1089@gmail.com>
2013-05-18 17:22:36 -0400

    Added fields and Javadoc to Product.

--- src/main/java/com/j2bugzilla/api/Product.java
+++ src/main/java/com/j2bugzilla/api/Product.java
@@ -13,12 +13,52 @@ public final class Product {
 
 	private final Optional<Integer> id;
 	
-	public Product(int id) {
+	private final String name;
+	
+	private final String description;
+	
+	/**
+	 * Creates a new {@link Product} which exists in a remote installation.
+	 * @param id An {@code int} which uniquely identifies this product.
+	 * @param name A {@code String} naming the product.
+	 * @param description A {@code String} describing the product.
+	 */
+	public Product(final int id, final String name, final String description) {
 		this.id = Optional.of(id);
+		this.name = name;
+		this.description = description;
 	}
 	
+	/**
+	 * Creates a new {@link Product} not associated with any remote installation.
+	 * @param name A {@code String} naming the product.
+	 * @param description A {@code String} describing the product.
+	 */
+	public Product(final String name, final String description) {
+		this.id = Optional.absent();
+		this.name = name;
+		this.description = description;
+	}
+	
+	/**
+	 * @return An {@link Optional} {@code Integer} identifying this {@link Product}.
+	 */
 	public Optional<Integer> getID() {
 		return id;
+	}
+	
+	/**
+	 * @return A {@code String} naming this {@link Product}.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @return A {@code String} describing this {@link Product}.
+	 */
+	public String getDescription() {
+		return description;
 	}
 	
 }
