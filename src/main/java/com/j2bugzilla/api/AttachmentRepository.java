5e99da33644809307b1e22d085f1f15cb50804d0
Tom Golden <TomRK1089@gmail.com>
2015-07-25 19:52:42 -0400

    Updated attachment repository to be stricter and throw an exception when the attachment does not exist.

--- src/main/java/com/j2bugzilla/api/AttachmentRepository.java
+++ src/main/java/com/j2bugzilla/api/AttachmentRepository.java
@@ -2,7 +2,6 @@ package com.j2bugzilla.api;
 
 import java.util.Set;
 
-import com.google.common.base.Optional;
 
 /**
  * The {@code AttachmentRepository} allows for existing {@link Attachment} objects to be retrieved and new
@@ -21,12 +20,11 @@ public interface AttachmentRepository {
 	int attachTo(Attachment attachment, Bug bug);
 	
 	/**
-	 * Retrieves an {@link Attachment} based on the given ID, or an absent {@link Optional} if the ID does not
-	 * exist in the remote Bugzilla installation.
+	 * Retrieves an {@link Attachment} based on the given ID, or throws an exception if no matching attachment exists.
 	 * @param id A {@code int} identifying a particular attachment.
-	 * @return An {@code Optional<Attachment>} object.
+	 * @return An {@code Attachment} object.
 	 */
-	Optional<Attachment> get(int id);
+	Attachment get(int id);
 	
 	/**
 	 * Retrieves all {@link Attachment} objects specified as a {@code Set}. If nonexistent IDs are included,
